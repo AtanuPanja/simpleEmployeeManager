5bf79ebcee55fc50cc6ffa5240409fb7e7fb8d76
AtanuPanja <74297632+AtanuPanja@users.noreply.github.com>
2020-11-25 20:22:08 +0530

    Add files via upload

---
+++ simple employee management.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int chars, i=0;
+	char* ptr;
+	while(i < 3)
+	{
+		printf("Employee %d\n", (i +1));
+		printf("Enter the number of characters in your Employee Id\n");
+		scanf("%d",&chars);
+		ptr = (char*) malloc((chars + 1) * sizeof(char));
+		printf("Enter your Employee Id\n");
+		scanf("%s", ptr);
+		printf("Your Employee Id is %s.\n",ptr);
+		free(ptr);
+		i = i + 1;
+	}
+	return 0;
+}
